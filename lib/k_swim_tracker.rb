c218b7aefd407cb4bafbe46ff6b7bce5e97b7787
David <david.cruwys@bugcrowd.com>
2020-01-13 19:19:07 +1100

    Initial Commit

---
+++ lib/k_swim_tracker.rb
@@ -0,0 +1,6 @@
+require "k_swim_tracker/version"
+
+module KSwimTracker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/k_swim_tracker/version.rb
@@ -0,0 +1,3 @@
+module KSwimTracker
+  VERSION = "0.1.0"
+end
